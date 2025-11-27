22a948efc7beeefad3c43af421a2804b2444f6c2
manash <pratimkashyapm@gmail.com>
2025-11-27 11:56:14 +0530

    another problem

--- array/index.js
+++ array/index.js
@@ -1,8 +1,17 @@
 // var x = [6 , 'manash', "kashyap"]
 
 
-var x = []
-x[0] = 1;
-x[1] = "manash";
-x[2] = "Kashyap"
-window.alert(x)
+// var x = []
+// x[0] = 1;
+// x[1] = "manash";
+// x[2] = "Kashyap"
+// window.alert(x)
+
+
+var btn = document.getElementById("btn");
+
+btn.onclick = function(){
+    var user_input = [];
+    user_input[0] = document.getElementById("user").value;  // use .value
+    window.alert(user_input)
+}
