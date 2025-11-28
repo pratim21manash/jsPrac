0c2ba3649df80832c3bc9d0aab28febc387036df
manash <pratimkashyapm@gmail.com>
2025-11-28 10:57:38 +0530

    first project

---
+++ loop/script.js
@@ -0,0 +1,14 @@
+var frm = document.getElementById("form")
+frm.onsubmit = function() {
+    var user_text = document.getElementById("text").value;
+    var user_num = document.getElementById("num").value;
+    var result = document.getElementById("result");
+
+    var i;
+    for(i=1; i<=user_num; i++){
+        // document.write(user_text + "<br>")
+        result.innerHTML = user_text;
+        return false;
+    }
+
+}
